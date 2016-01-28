3ec5f986e849e3fdda0e2715e08439387af2b1ad
Michael Wolfenden <michael.wolfenden@gmail.com>
2016-01-28 16:30:26 +1000

    use teaspoon for react component testing

--- client/src/app/components/Voting_spec.jsx
+++ client/src/app/components/Voting_spec.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
+import $ from 'teaspoon';
 
 import Voting from './Voting';
 
 describe('Voting', () => {
-
     it('renders a pair of buttons', () => {
-        const renderer = TestUtils.createRenderer();
-        renderer.render(<Voting pair={['Trainspotting', '28 Days Later']} />);
+        const $component = $(
+            <Voting pair={['Trainspotting', '28 Days Later']} />
+        ).render();
 
-        const actual = renderer.getRenderOutput();
+        const $buttons = $component.find('button');
 
-        expect(actual).to.include(<button><h1>Trainspotting < /h1></button >);
-        expect(actual).to.include(<button><h1>28 Days Later< /h1></button >);
+        expect($buttons.length).to.equal(2);
+        expect($buttons.first().text()).to.equal('Trainspotting');
+        expect($buttons.last().text()).to.equal('28 Days Later');
+    });
+
+    it('invokes callback when a button is clicked', () => {
+        let votedWith;
+        const vote = (entry) => votedWith = entry;
+
+        const $component = $(
+            <Voting pair={['Trainspotting', '28 Days Later']} vote={vote} />
+        ).render();
+
+        $component
+            .find('button')
+            .first()
+            .trigger('click');
+
+        expect(votedWith).to.equal('Trainspotting');
     });
 });
